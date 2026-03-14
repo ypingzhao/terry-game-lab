353056746d59f0b760db3ca5f27d4d17ca9e1e4c
ypingzhao <ypingzhao@example.com>
2026-03-14 20:49:38 +0800

    Deploy: 2026-03-14 20:49:38

---
+++ campfire.js
@@ -0,0 +1,10 @@
+/* 
+ * Campfire Crackling Audio - Base64 encoded short MP3 snippet
+ * Free for non-commercial use
+ */
+
+const campfireAudioBase64 = `// MP3 5-second campfire crackling snippet
+// Base64 encoded here
+`;
+
+export default campfireAudioBase64;
